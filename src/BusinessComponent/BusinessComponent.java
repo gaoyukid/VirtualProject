10b84d227ffbf324585005710a6db02172e109da
gaoyukid <gaoyu.kid@gmail.com>
2013-03-09 15:54:10 +0800

    create

---
+++ src/BusinessComponent/BusinessComponent.java
@@ -0,0 +1,13 @@
+package BusinessComponent;
+
+public class BusinessComponent {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/TestCase/testdebug.java
@@ -0,0 +1,13 @@
+package TestCase;
+
+public class testdebug {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
